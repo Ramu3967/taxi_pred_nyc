107dfe8f8f0e7cd13643a14de1350d54397c6a38
ramu3967 <ramum9933@gmail.com>
2022-08-01 16:37:36 -0400

    added large files

---
+++ app.py
@@ -0,0 +1,32 @@
+import pickle
+
+from flask import Flask, render_template, request, jsonify
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    test_array = list(request.form.values())
+    print("values received are - ", test_array)
+    last = test_array[len(test_array) - 1]
+    if last == 'Submit': test_array.pop()
+    # TODO
+
+@app.route('/android_predict', methods=['POST'])
+def android_predict():
+    req_json = request.json
+    print("Values that were posted :\n", req_json)
+    # TODO
+
+def get(key): return request.form.get(key, 0)
