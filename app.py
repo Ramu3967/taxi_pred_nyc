02f0db2a6b9683391e667c6b6086de5937a2e228
Ramu Mandaloju <ramum9933@gmail.com>
2022-08-02 02:51:32 +0530

    removed api definitions

--- app.py
+++ app.py
@@ -15,18 +15,18 @@ def hello_world():  # put application's code here
 if __name__ == '__main__':
     app.run()
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    test_array = list(request.form.values())
-    print("values received are - ", test_array)
-    last = test_array[len(test_array) - 1]
-    if last == 'Submit': test_array.pop()
-    # TODO
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     test_array = list(request.form.values())
+#     print("values received are - ", test_array)
+#     last = test_array[len(test_array) - 1]
+#     if last == 'Submit': test_array.pop()
+#     # TODO
 
-@app.route('/android_predict', methods=['POST'])
-def android_predict():
-    req_json = request.json
-    print("Values that were posted :\n", req_json)
-    # TODO
+# @app.route('/android_predict', methods=['POST'])
+# def android_predict():
+#     req_json = request.json
+#     print("Values that were posted :\n", req_json)
+#     # TODO
 
 def get(key): return request.form.get(key, 0)
